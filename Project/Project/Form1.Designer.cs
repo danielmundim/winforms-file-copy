fb21c487ba6427f4108d15fe039d845fdf0b1e44
Daniel Mundim <dmundim.cr@gmail.com>
2016-11-17 21:11:27 -0300

    Innitial commit

---
+++ Project/Project/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFiles = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblFiles
+            // 
+            this.lblFiles.AutoSize = true;
+            this.lblFiles.Location = new System.Drawing.Point(17, 16);
+            this.lblFiles.Name = "lblFiles";
+            this.lblFiles.Size = new System.Drawing.Size(0, 13);
+            this.lblFiles.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Copy Files";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.lblFiles);
+            this.panel1.Location = new System.Drawing.Point(12, 116);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(410, 382);
+            this.panel1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Source";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Target";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Source Folder";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Target Folder";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 510);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFiles;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Project/Project/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project
+{
+    public partial class Form1 : Form
+    {
+        private string SourceFolder { get; set; }
+        private string TargetFolder { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Call(string source, string target)
+        {
+            lblFiles.Text = string.Empty;
+
+            if (!System.IO.Directory.Exists(target))
+            {
+                System.IO.Directory.CreateDirectory(target);
+            }
+
+
+            if (Directory.Exists(source))
+            {
+                var dirs = Directory.GetDirectories(source);
+                
+                for (int i = 0; i < dirs.Length; i++)
+                {
+                    string folder = Path.GetFileName(Path.GetDirectoryName(dirs[i] + "\\"));
+
+                    if (!Directory.Exists(folder))
+                    {
+                        Directory.CreateDirectory(target + "\\" + folder);
+                    }
+
+                    CopyFiles(dirs[i], target + "\\" + folder);
+                }
+               CopyFiles(source, target);
+            }
+
+            MessageBox.Show("Done!");
+
+        }
+
+        private void CopyFiles(string dir, string target)
+        {
+            string[] files = System.IO.Directory.GetFiles(dir);
+            foreach (string s in files)
+            {
+                string fileName = System.IO.Path.GetFileName(s);
+                string destFile = System.IO.Path.Combine(target, fileName);
+                if (!File.Exists(destFile))
+                {
+                    File.Copy(s, destFile, false);
+                    lblFiles.Text += destFile + Environment.NewLine;
+                    lblFiles.Refresh();
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(SourceFolder) && !string.IsNullOrEmpty(TargetFolder))
+            {
+                Call(SourceFolder, TargetFolder);
+            }
+            else
+            {
+                MessageBox.Show("You must select a source and a target folder to continue!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog(); 
+            if (result == DialogResult.OK)
+            {
+                SourceFolder = folderBrowserDialog1.SelectedPath;
+                label1.Text = SourceFolder;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog(); 
+            if (result == DialogResult.OK)
+            {
+                TargetFolder = folderBrowserDialog1.SelectedPath;
+                label2.Text = TargetFolder;
+            }
+        }
+    }
+}
